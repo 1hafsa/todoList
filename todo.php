d7641576ddbf6480584dedcd4aa26f8a9c241946
1hafsa <hafcatiko@gmail.com>
2024-01-06 15:33:13 +0100

    new commit

--- todo.php
+++ todo.php
@@ -9,9 +9,9 @@
 <?php
 
 $host = "localhost";
-$dbname = "votre_base_de_donnees";
-$user = "votre_utilisateur";
-$password = "votre_mot_de_passe";
+$dbname = "todolist";
+$user = "root";
+$password = "";
 
 try {
     $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
@@ -19,41 +19,74 @@ try {
     die('Erreur : ' . $e->getMessage());
 }
 
-// Récupération des tâches depuis la base de données
 $req = $bdd->query('SELECT * FROM todo ORDER BY created_at DESC');
 $taches = $req->fetchAll(PDO::FETCH_ASSOC);
 
-// Traitement des actions
+
 if (isset($_POST['action'])) {
     $action = $_POST['action'];
     $id = $_POST['id'];
 
     switch ($action) {
         case 'new':
-            // Ajouter une nouvelle tâche
+            
             $title = $_POST['title'];
             $stmt = $bdd->prepare('INSERT INTO todo (title, created_at) VALUES (?, NOW())');
             $stmt->execute([$title]);
             break;
 
         case 'delete':
-            // Supprimer la tâche
+         
             $stmt = $bdd->prepare('DELETE FROM todo WHERE id = ?');
             $stmt->execute([$id]);
             break;
 
         case 'toggle':
-            // Basculer la valeur du champ "done"
+         
             $stmt = $bdd->prepare('UPDATE todo SET done = 1 - done WHERE id = ?');
             $stmt->execute([$id]);
             break;
     }
-
-    // Redirection pour éviter la réexécution du formulaire lors de la actualisation de la page
-    header('Location: index.php');
+    header('Location:todo.php');
+   
     exit();
 }
 ?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Todo App</a>
+</nav>
+
+
+<div class="container mt-4">
+    <form method="post">
+        <div class="form-group">
+            <label for="title">Nouvelle tâche</label>
+            <input type="text" class="form-control" id="title" name="title" required>
+        </div>
+        <button type="submit" class="btn btn-primary" name="action" value="new">Ajouter</button>
+    </form>
+</div>
+
+
+<div class="container mt-4">
+    <ul class="list-group">
+        <?php foreach ($taches as $tache): ?>
+            <li class="list-group-item <?php echo ($tache['done'] == 1) ? 'list-group-item-success' : 'list-group-item-warning'; ?>">
+                <?php echo $tache['title']; ?>
+                <form class="float-right" method="post">
+                    <input type="hidden" name="id" value="<?php echo $tache['id']; ?>">
+                    <button type="submit" class="btn btn-success" name="action" value="toggle">Toggle</button>
+                    <button type="submit" class="btn btn-danger" name="action" value="delete">Supprimer</button>
+                </form>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.css"></script>
     
 </body>
 </html>
